3dd7d8ff984f9226a794e8f2416aab128279696c
Cyrill Schumacher <cyrill@schumacher.fm>
2014-10-10 11:22:04 +1100

    Update Varnish 4.0 Dockerfile

--- Varnish/boot.sh
+++ Varnish/boot.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-
-varnishd -a $LISTEN_ADDR:$LISTEN_PORT -T $TELNET_ADDR:$TELNET_PORT -f $VCL_FILE -s file,/var/cache/varnish.cache,$CACHE_SIZE -F
+varnishd -a $LISTEN_ADDR:$LISTEN_PORT -T $TELNET_ADDR:$TELNET_PORT -s file,/var/cache/varnish/varnish.cache,$CACHE_SIZE -F
