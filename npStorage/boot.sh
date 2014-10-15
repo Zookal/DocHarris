f80e5e0437796921832ebb955d18f4fbc7677799
Cyrill Schumacher <cyrill@schumacher.fm>
2014-10-15 14:35:29 +1100

    npStorage: Correct permissions of folders

--- npStorage/boot.sh
+++ npStorage/boot.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+chown -R root /var/www/data/nginx /var/www/data/php
+chgrp -R root /var/www/data/nginx /var/www/data/php
+chmod 770 /var/www/data/nginx /var/www/data/php
+chgrp -R www-data /var/www/data/media /var/www/data/sitemap /var/www/data/var
+chmod 775 /var/www/data/media /var/www/data/sitemap /var/www/data/var
+
 rm -Rf /var/www/site/htdocs/media
 rm -Rf /var/www/site/htdocs/sitemap
 rm -Rf /var/www/site/htdocs/var
